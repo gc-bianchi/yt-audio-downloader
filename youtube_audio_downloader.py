228659fce2fdae38e3962e73c64843e727b7de56
gc-bianchi <72824902+gc-bianchi@users.noreply.github.com>
2024-09-18 20:17:27 -0600

    base file

---
+++ youtube_audio_downloader.py
@@ -0,0 +1,10 @@
+def download_youtube_content(youtube_search_term: List, content_dir: str) -> bool:
+    """
+    Use a list of video or playlist (urls) URLs and/or search (search_terms) YouTube for videos.
+    Find the videos, download the audio of the video.
+
+    :param youtube_search_term:     A list of discrete urls or search terms
+    :param content_dir:             The directory to place the downloaded YouTube content
+    :return:                        Success or Failure
+    """
+    return True
